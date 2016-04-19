522a97fe84700d1f9abaf8b6c77cda552778b7f3
wlane <will@asklane.com>
2016-04-19 13:25:34 -0700

    switched from running annotation pipeline to tempeval2013 pipeline

--- src/main/java/RelationExtractor.java
+++ src/main/java/RelationExtractor.java
@@ -3,13 +3,19 @@
  */
 import org.cleartk.examples.pos.RunExamplePOSAnnotator;
 import org.cleartk.timeml.*;
+import org.cleartk.eval.*;
 import org.cleartk.examples.*;
+import org.cleartk.timeml.eval.TempEval2013Evaluation;
 
 public class RelationExtractor {
     public static void main(String[] args){
         System.out.print("Running TempEval 2013 ClearTK-TimeML system");
         try {
-            TimeMlAnnotate.main("src/main/resources/TE3-platinum-test");
+//            --train-dirs /path/to/TimeBank /path/to/AQUAINT
+//            --test-dirs /path/to/te3-platinum
+            String[] in_out = {"--train-dirs", "src/main/resources/train/TBAQ-cleaned/TimeBank","src/main/resources/train/TBAQ-cleaned/AQUAINT", "--test-dirs","src/main/resources/TE3-platinum-test"};
+            TempEval2013Evaluation.main(in_out);
+            //TimeMlAnnotate.main("src/main/resources/TE3-platinum-test");
         } catch (Exception e) {
             e.printStackTrace();
         }
