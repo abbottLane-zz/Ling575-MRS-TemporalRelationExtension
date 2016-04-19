582c7f169f89ab789240721de5096e72ff1c5866
wlane <will@asklane.com>
2016-04-19 15:59:48 -0700

    fixed reference to training data

--- src/main/java/RelationExtractor.java
+++ src/main/java/RelationExtractor.java
@@ -1,10 +1,3 @@
-/**
- * Created by wlane on 4/18/16.
- */
-import org.cleartk.examples.pos.RunExamplePOSAnnotator;
-import org.cleartk.timeml.*;
-import org.cleartk.eval.*;
-import org.cleartk.examples.*;
 import org.cleartk.timeml.eval.TempEval2013Evaluation;
 
 public class RelationExtractor {
@@ -13,7 +6,7 @@ public class RelationExtractor {
         try {
 //            --train-dirs /path/to/TimeBank /path/to/AQUAINT
 //            --test-dirs /path/to/te3-platinum
-            String[] in_out = {"--train-dirs", "src/main/resources/train/TBAQ-cleaned/TimeBank","src/main/resources/train/TBAQ-cleaned/AQUAINT", "--test-dirs","src/main/resources/TE3-platinum-test"};
+            String[] in_out = {"--train-dirs", "src/main/resources/train/TBAQ-cleaned/TimeBank","src/main/resources/train/TBAQ-cleaned/AQUAINT", "--test-dirs","src/main/resources/te3-platinum"};
             TempEval2013Evaluation.main(in_out);
             //TimeMlAnnotate.main("src/main/resources/TE3-platinum-test");
         } catch (Exception e) {
