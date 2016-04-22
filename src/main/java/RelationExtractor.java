40ef1bb1e752b4a72b1cc3ba695c800334e2a011
wlane <will@asklane.com>
2016-04-21 19:20:43 -0700

    added dev-test corpus

--- src/main/java/RelationExtractor.java
+++ src/main/java/RelationExtractor.java
@@ -6,7 +6,7 @@ public class RelationExtractor {
         try {
 //            --train-dirs /path/to/TimeBank /path/to/AQUAINT
 //            --test-dirs /path/to/te3-platinum
-            String[] in_out = {"--train-dirs", "src/main/resources/train/TBAQ-cleaned/TimeBank","src/main/resources/train/TBAQ-cleaned/AQUAINT", "--test-dirs","src/main/resources/te3-platinum"};
+            String[] in_out = {"--train-dirs", "src/main/resources/train/TBAQ-cleaned/TimeBank","src/main/resources/train/TBAQ-cleaned/AQUAINT", "--test-dirs","src/main/resources/train/TBAQ-cleaned/dev-test"};
             TempEval2013Evaluation.main(in_out);
         } catch (Exception e) {
             e.printStackTrace();
