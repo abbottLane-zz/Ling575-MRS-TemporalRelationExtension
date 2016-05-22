2f8ad042eb649a7885f71fe1b6abec4b74be1ad2
mrtnhorn <mrtnhorn@me.com>
2016-05-21 23:34:07 -0700

    Updated feature extractor Handles sentences that won't parse as well as cyclical graph searches

--- src/main/resources/PythonScripts/feature_extractor.py
+++ src/main/resources/PythonScripts/feature_extractor.py
@@ -1,6 +1,6 @@
 #!/opt/python-3.4/bin/python3
 
-"""%prog [--help] <e1> <e1_begin> <e1_end> <e2> <e2_begin> <e2_end> [<sent_file>]\n"""
+"""%prog [--help] <e1> <e1_begin> <e1_end> <e2> <e2_begin> <e2_end> [<"sentence">]"""
 
 
 import sys
@@ -21,10 +21,10 @@ def extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, ace_result):
     mrs = simplemrs.loads_one(ace_result)
     ep_1, ep_2 = None, None
     for ep in mrs.eps():
-        if ep.cfrom <= e1_b < ep.cto:
+        if ep.cfrom <= e1_b < ep.cto and (ep.pred.pos == 'n' or ep.pred.pos == 'v'):
             ep_1 = ep
             continue
-        elif ep.cfrom <= e2_b < ep.cto:
+        elif ep.cfrom <= e2_b < ep.cto and (ep.pred.pos == 'n' or ep.pred.pos == 'v'):
             ep_2 = ep
             break
     if ep_1 and ep_2:
@@ -36,7 +36,7 @@ def extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, ace_result):
             feat = "e1:" + prop + ":" + mrs.properties(ep_1.iv)[prop]
             features.append(feat)
         for prop in mrs.properties(ep_2.iv):
-            feat = "e2:" + prop + ":" + mrs.properties(ep_1.iv)[prop]
+            feat = "e2:" + prop + ":" + mrs.properties(ep_2.iv)[prop]
             features.append(feat)
         args_1 = mrs.args(ep_1.nodeid)
         args_2 = mrs.args(ep_2.nodeid)
@@ -59,38 +59,57 @@ def extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, ace_result):
             for path in paths:
                 if len(path.split(",")) < len(shortest_path.split(",")):
                     shortest_path = path
-            features.append("PATH:" + shortest_path)
+        else:
+            shortest_path = "PATH:NO_PATH"
+        features.append("PATH:" + shortest_path)
 
     return features
+
+
+seen_ids = set()
 
 
 def find_paths(mrs, ep_1, ep_2, cur_path):
     # Recursive path search using incoming args of second event
     paths = []
     if len(cur_path) == 0:
-        cur_path = ep_2.pred.pos
+        if ep_2.pred.pos:
+            cur_path = ep_2.pred.pos
+        else:
+            cur_path = "NO_POS"
         # Debug line: pred name
         # cur_path = ep_2.pred.string.strip("\"")
     else:
-        cur_path = ep_2.pred.pos + "," + cur_path
+        if ep_2.pred.pos:
+            cur_path = ep_2.pred.pos + "," + cur_path
+        else:
+            cur_path = "NO_POS," + cur_path
         # Debug line: pred name
         # cur_path = ep_2.pred.string.strip("\"") + "," + cur_path
     in_args = mrs.incoming_args(ep_2.nodeid)
 
     for node_id in in_args:
-        if mrs.ep(node_id).label != ep_1.label and mrs.ep(node_id).iv != ep_1.iv:
-            for arg in in_args[node_id]:
-                prefix = arg + ":"
-                break
-            paths += find_paths(mrs, ep_1, mrs.ep(node_id), prefix+cur_path)
+        global seen_ids
+        if node_id not in seen_ids:
+            if mrs.ep(node_id).label != ep_1.label and mrs.ep(node_id).iv != ep_1.iv:
+                for arg in in_args[node_id]:
+                    prefix = arg + ":"
+                    break
+                seen_ids.add(node_id)
+                paths += find_paths(mrs, ep_1, mrs.ep(node_id), prefix+cur_path)
+            else:
+                for arg in in_args[node_id]:
+                    prefix = arg + ":"
+                    break
+                if mrs.ep(node_id).pred.pos:
+                    cur_path = mrs.ep(node_id).pred.pos + "," + prefix + cur_path
+                else:
+                    cur_path = "NO_POS," + prefix + cur_path
+                # Debug line: pred name
+                # cur_path = mrs.ep(node_id).pred.string.strip("\"") + "," + prefix + cur_path
+                paths.append(cur_path)
         else:
-            for arg in in_args[node_id]:
-                prefix = arg + ":"
-                break
-            cur_path = mrs.ep(node_id).pred.pos + "," + prefix + cur_path
-            # Debug line: pred name
-            # cur_path = mrs.ep(node_id).pred.string.strip("\"") + "," + prefix + cur_path
-            paths.append(cur_path)
+            paths.clear()
 
     return paths
 
@@ -98,7 +117,11 @@ def find_paths(mrs, ep_1, ep_2, cur_path):
 def run_ace(sentence):
     ace_bin = "/Applications/ace/ace-0.9.22/ace"
     erg_file = "/Applications/ace/ace-0.9.22/erg-1214-osx-0.9.22.dat"
-    res1 = ace.parse(erg_file, sentence, cmdargs=['-n', '1'], executable=ace_bin)['RESULTS'][0]['MRS']
+    results = ace.parse(erg_file, sentence, cmdargs=['-n', '1'], executable=ace_bin)['RESULTS']
+    if results:
+        res1 = ace.parse(erg_file, sentence, cmdargs=['-n', '1'], executable=ace_bin)['RESULTS'][0]['MRS']
+    else:
+        res1 = None
 
     return res1
 
@@ -122,10 +145,11 @@ def read_doc(e1, e1_begin, e1_end, e2, e2_begin, e2_end, file_name=None):
 
     # Check for specified input file
     if file_name:
-        # Read sentence from file
-        with open(file_name, 'r') as doc:
-            doc_text = doc.read()
-            sent = doc_text.strip()
+        sent = file_name
+        # # Read sentence from file
+        # with open(file_name, 'r') as doc:
+        #     doc_text = doc.read()
+        #     sent = doc_text.strip()
     else:
         # Read sentence from stdin
         sent = input()
@@ -143,16 +167,22 @@ if __name__ == "__main__":
                                                         args[2], args[3],
                                                         args[4], args[5])
         res = run_ace(sent)
-        feats = extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, res)
-        output_features(feats)
+        if res:
+            feats = extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, res)
+            output_features(feats)
+        else:
+            print("NO_PARSE")
     if len(args) == 7:
         e1, e1_b, e1_e, e2, e2_b, e2_e, sent = read_doc(args[0], args[1],
                                                         args[2], args[3],
                                                         args[4], args[5],
                                                         file_name=args[6])
         res = run_ace(sent)
-        feats = extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, res)
-        output_features(feats)
+        if res:
+            feats = extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, res)
+            output_features(feats)
+        else:
+            print("NO_PARSE")
     else:
         sys.stderr.write("Must specify 6/7 arguments: <e1> <e1_begin> <e1_end> "
-                         "<e2> <e2_begin> <e2_end> [<sent_file>]\n")
+                         "<e2> <e2_begin> <e2_end> [<\"sentence\">]\n")
