d868ebebeef5ef91f4199381984b152aa5f94afd
wlane <will@asklane.com>
2016-06-01 18:30:42 -0700

    collected cached sentences

--- src/main/java/org/cleartk/timeml/uw/feature/semantics/SemanticPathBetweenEventsExtractor.java
+++ src/main/java/org/cleartk/timeml/uw/feature/semantics/SemanticPathBetweenEventsExtractor.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+import com.sun.org.apache.xml.internal.serializer.utils.SystemIDResolver;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import edu.stanford.nlp.trees.LabeledScoredTreeFactory;
 import edu.stanford.nlp.trees.PennTreeReader;
 import edu.stanford.nlp.trees.Tree;
@@ -39,8 +41,6 @@ import com.google.common.base.Joiner;
 public class SemanticPathBetweenEventsExtractor<T extends Annotation, U extends Annotation> implements FeatureExtractor2<T, U> {
     public List<Feature> extract(JCas jCas, T source, U target) {
 
-        List<Feature> features = new ArrayList<Feature>();
-
         //Derive the sentence from which the events come
         TreebankNode sourceNode = TreebankNodeUtil.selectMatchingLeaf(jCas, source);
         TreebankNode topNode = TreebankNodeUtil.getTopNode(sourceNode);
@@ -56,6 +56,9 @@ public class SemanticPathBetweenEventsExtractor<T extends Annotation, U extends 
             originalSentence = originalSentence.replace("\'\'", "");
             originalSentence = originalSentence.replace("_", "");
             originalSentence = originalSentence.replace("``", "");
+            originalSentence = originalSentence.replace("-RRB-", "");
+            originalSentence = originalSentence.replace("dlrs", "$");
+            originalSentence = originalSentence.replace("  ", " ");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -68,32 +71,21 @@ public class SemanticPathBetweenEventsExtractor<T extends Annotation, U extends 
         int e2_begin = originalSentence.indexOf(e2);
         int e2_end = originalSentence.indexOf(e2)+e2.length();
 
-        // in cache mode, write all sentences to a file
-        String cached_sent_filename = "/home/wlane/IdeaProjects/Ling575-MRS-TemporalRelationExtension/src/main/resources/CachedData/cachedSentences.out";
-        Writer output;
-        try {
-            output = new BufferedWriter(new FileWriter(cached_sent_filename, true));
-            StringBuilder sb = new StringBuilder();
-            sb.append(e1);
-            sb.append("#-#");
-            sb.append(e1_begin);
-            sb.append("#-#");
-            sb.append(e1_end);
-            sb.append("#-#");
-            sb.append(e2);
-            sb.append("#-#");
-            sb.append(e2_begin);
-            sb.append("#-#");
-            sb.append(e2_end);
-            sb.append("#-#");
-            sb.append(originalSentence);
-            sb.append("\n");
-            output.append(sb.toString());
-            output.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // in cache-prep mode, write all sentences to a file
+        String cached_sent_filename = "/home/wlane/IdeaProjects/Ling575-MRS-TemporalRelationExtension/src/main/resources/CachedData/cachedSentences.dev.FULL.out";
+        cacheSentences(cached_sent_filename, e1,e1_begin,e1_end,e2,e2_begin,e2_end,originalSentence);
 
+        // for current sentence, read in the cached feature set using a key
+        StringBuilder key = new StringBuilder();
+        key.append(e1);
+        key.append(e1_begin);
+        key.append(e1_end);
+        key.append(e2);
+        key.append(e2_begin);
+        key.append(e2_end);
+        String key_str = key.toString();
+
+        String cachedFeatureDictDir = "src/main/resources/CachedData/cachedFeatureDictionary.eval.base.out";
 
 //        // Create python cmd line command
 //        String line = "python3 /home/wlane/IdeaProjects/Ling575-MRS-TemporalRelationExtension/src/main/resources/PythonScripts/feature_extractor.py "+ e1 + " " + e1_begin + " " +e1_end + " " + e2+ " " + e2_begin + " " + e2_end + " \""+ originalSentence + "\"";
@@ -126,8 +118,81 @@ public class SemanticPathBetweenEventsExtractor<T extends Annotation, U extends 
 //                //System.out.println("NAME:" + name_val[0] + " " + "VAL:" + name_val[1] + " ");
 //            }
 //        }
+        return  getFeatureList(key_str, cachedFeatureDictDir);
+    }
+
+    private List<Feature> getFeatureList(String key_str, String cachedFeatureDictDir) {
+
+        // First load the dictionary
+        //HashMap<String, String> featureDict = new HashMap<String, String>();
+        String featurs_str = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(cachedFeatureDictDir))) {
+            String line;
+            Boolean key_found = false;
+            while ((line = br.readLine()) != null) {
+                // process the line.
+                String[] key_val = line.split("=:=");
+                if(key_val[0].equals(key_str)){
+                    featurs_str = key_val[1];
+                    key_found = true;
+                }
+            }
+            if(!key_found)
+            {
+                System.out.println("KEY NOT FOUND !!!: " + key_str);
+                System.out.println();
+                List<Feature> not_found = new ArrayList<>();
+                not_found.add(new Feature("NOT_FOUND", "NOT_FOUND"));
+                return not_found;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] feat_toks = featurs_str.split(" ");
+
+        // create Feature objects
+        List<Feature> features = new ArrayList<>();
+        for (String feature : feat_toks) {
+            if(!feature.equals("NO_PARSE") && !feature.equals("NO_FEATS")) {
+                String[] name_val = feature.split("=");
+                if (name_val[0].contains("/usr/") || name_val[1].contains("/usr/")) {
+                    break;
+                }
+                features.add(new Feature(name_val[0], name_val[1]));
+                //System.out.println("NAME:" + name_val[0] + " " + "VAL:" + name_val[1] + " ");
+            }
+        }
         return features;
     }
+
+    private void cacheSentences(String cached_sent_filename, String e1, int e1_begin, int e1_end, String e2, int e2_begin, int e2_end, String originalSentence) {
+        Writer output;
+        try {
+            output = new BufferedWriter(new FileWriter(cached_sent_filename, true));
+            StringBuilder sb = new StringBuilder();
+            sb.append(e1);
+            sb.append("#-#");
+            sb.append(e1_begin);
+            sb.append("#-#");
+            sb.append(e1_end);
+            sb.append("#-#");
+            sb.append(e2);
+            sb.append("#-#");
+            sb.append(e2_begin);
+            sb.append("#-#");
+            sb.append(e2_end);
+            sb.append("#-#");
+            sb.append(originalSentence);
+            sb.append("\n");
+            output.append(sb.toString());
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public String execToString(String command) throws Exception {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         CommandLine commandline = CommandLine.parse(command);
@@ -142,22 +207,23 @@ public class SemanticPathBetweenEventsExtractor<T extends Annotation, U extends 
     }
     private String getOriginalSentence(List<Tree> toks) throws IOException {
 
-//        String[] str_toks = new String[toks.size()];
-//        int i = 0;
-//        for(Tree tok : toks)
-//        {
-//            str_toks[i] = tok.toString();
-//            i++;
-//        }
-
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i < toks.size(); i++){
 
             sb.append(toks.get(i).toString());
-
             if(i+1 < toks.size()){
-                if(!toks.get(i+1).toString().equals(",") && !toks.get(i+1).toString().equals("'") && !toks.get(i+1).toString().equals(".") && !toks.get(i+1).toString().equals("n\'t") && !toks.get(i+1).toString().equals("\'s") && !toks.get(i+1).toString().equals("\'ll"))
+                if(!toks.get(i+1).toString().equals(",")
+                        && !toks.get(i).toString().equals("$")
+                        && !toks.get(i+1).toString().equals("'")
+                        && !toks.get(i+1).toString().equals(".")
+                        && !toks.get(i+1).toString().equals("n\'t")
+                        && !toks.get(i+1).toString().equals("\'s")
+                        && !toks.get(i+1).toString().equals("\'ll")
+                        && !toks.get(i+1).toString().equals("\'m")
+                        && !toks.get(i+1).toString().equals("\'re")
+                        && !toks.get(i+1).toString().equals("%")
+                        )
                 sb.append(" ");
             }
         }
